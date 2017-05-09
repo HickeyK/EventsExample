679fe379c7d88774bd214d6fd7e8ae52b5117dfb
Kevin Hickey <kevin.hickey@aberdeen-asset.com>
2017-05-09 12:12:26 +1000

    Add project files.

---
+++ EventsExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EventsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+
+            worker.WorkDone += new WorkDoneEventHandler(Worker_WorkDone);
+            worker.WorkComplete += new EventHandler(Worker_WorkComplete);
+
+            worker.DoWork(10);
+        }
+
+
+        private static void Worker_WorkDone(int i)
+        {
+            Console.WriteLine("Done work {0}", i.ToString());
+        }
+
+        private static void Worker_WorkComplete(Object Sender, EventArgs e)
+        {
+            Console.WriteLine("Work Complete");
+        }
+
+    }
+}
---
+++ EventsExample/Worker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EventsExample
+{
+
+    public delegate void WorkDoneEventHandler(int i);
+
+    class Worker
+    {
+
+        public event WorkDoneEventHandler WorkDone;
+        public event EventHandler WorkComplete;
+
+
+        public void DoWork(int i)
+        {
+            for (int count = 0; count < i; count++)
+            {
+                OnWorkDone(count);
+            }
+            OnWorkCompleted();
+        }
+
+
+        protected virtual void OnWorkDone(int i)
+        {
+            var del = WorkDone as WorkDoneEventHandler;
+            if (del != null)
+            {
+                del(i);
+            }
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            var del = WorkComplete as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+        }
+
+
+
+
+    }
+}
